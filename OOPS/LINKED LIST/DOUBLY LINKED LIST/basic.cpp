dcefedbcdbb448aaf99885074b66da508d6154e4
Dev <97741424+devrajchhikara@users.noreply.github.com>
2024-04-14 17:29:45 +0000

    .

---
+++ OOPS/LINKED LIST/DOUBLY LINKED LIST/basic.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val){
+    node* n = new node(val);
+        
+    n->next = head;
+    if(head != NULL){
+        head->prev = n;
+    }
+    head = n;
+}
+
+void insertAtTail(node* &head, int val){
+    node* n = new node(val);
+
+    node* temp = head;
+    
+    if (head == NULL){
+        insertAtHead(head, val);
+        return;
+    }
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+
+    temp->next = n;
+    n->prev = temp;
+}
+
+void display(node* head){
+    node* temp = head;
+    cout << "NULL  ";
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp ->next;
+    }
+    cout << "  NULL" << endl;
+    
+}
+
+void deleteAtHead(node* head){
+    node* todelete = head;
+    head = head->next;
+    head->prev = NULL;
+
+    delete todelete;
+}
+
+void Delete(node* &head, int pos){
+    node* temp = head;
+    
+    if(pos == 1){
+        deleteAtHead(head);
+        return;
+    }
+    int count = 1;
+    while (temp != NULL && count != pos){
+        temp = temp->next;
+        count++;
+    }
+    
+    
+    temp->prev->next = temp->next;
+    if(temp->next != NULL){
+        temp->next->prev = temp->prev;
+    }
+    
+
+    delete temp;
+}
+
+int main(){
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtHead(head, 5);
+    insertAtHead(head, 6);
+    display(head);
+    Delete(head, 4);
+    display(head);
+    Delete(head, 1);
+    display(head);
+    return 0;
+}
+hiuhfERROR ERROR
